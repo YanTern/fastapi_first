fcbd457f7ace2d0e199dd2306197308f2fec229a
George <e.shegolihin2010@yandex.ru>
2026-05-20 15:30:00 +0300

    get / post

--- app/main.py
+++ app/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/hotels")
-def get_hotels():
-    return "Hotel resort"
+class HotelsSearchArgs:
+    def __init__(
+        self,
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location 
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
 
+
+@app.get("/hotels")
+def get_hotels(search_args: HotelsSearchArgs = Depends()):
+    return search_args
